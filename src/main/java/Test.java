eaf26babf3dd53ea6a1a7dfb6a50d47d10a5aa8f
hx <hengxuan.dong@gmail.com>
2021-01-22 01:40:22 -0800

    backup

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,16 +1,17 @@
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 import org.json.JSONObject;
 import org.json.JSONPointer;
 import org.json.XML;
 
 public class Test {
-    public static void main(String args[]) throws FileNotFoundException {
-        JSONObject jo = new JSONObject("{ \"abc\" : \"def\" }");
-        System.out.println(jo.toString());
-        JSONPointer jp = new JSONPointer("/CATALOG");
-        FileReader r = new FileReader("sample1.xml");
-        JSONObject newJo = XML.toJSONObject(r, jp);
-        System.out.println(newJo.toString());
-    }
+	public static void main(String args[]) throws FileNotFoundException {
+		JSONObject jo = new JSONObject("{ \"abc\" : \"def\" }");
+		System.out.println(jo.toString());
+		JSONPointer jp = new JSONPointer("/letter/title");
+		FileReader r = new FileReader("sample2.xml");
+		JSONObject newJo = XML.toJSONObject(r, jp);
+		System.out.println(newJo.toString());
+	}
 }
