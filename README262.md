fe14440a7a300fa6454931430c8d8d9bda79892f
Nina Vuong <ninavuong@ninas-air.lan>
2021-02-09 16:11:33 -0800

    add test

---
+++ README262.md
@@ -0,0 +1,9 @@
+Milestone 3:
+run the file test in JSON-java/src/test/java/org.json.junit/XMLTest262.java
+
+@Test
+taskTest4 to test new function that that input as String and return another String that is the transformation of the key.
+
+Performance implications of doing this inside the library is much better (faster) than doing it in client code in Milestone1
+After use System.currentTimeMillis(); to calculate the performance time. 
+That just took 9ms to run for Milestone3. But took 22ms to run for Milestone1
--- src/test/java/org/json/junit/XMLTest262.java
+++ src/test/java/org/json/junit/XMLTest262.java
@@ -141,66 +141,20 @@ public class XMLTest262 {
 				+ "  <logo alt=\"Logo of Smith and Company Ltd.\" address=\"http://www.smith-company-ltd.com/logo.jpg\"/>\n"
 				+ "    <signature>Nina Vuong</signature>\n" + "</letter>\n" + "<letter1>\n" + "</letter1>";
 
-
-        String expectedJsonString = "{\n" + "  \"letter\": {\n" + "    \"address\": [\n" + "      {\n"
-                + "        \"translate\": \"yes\",\n" + "        \"content\": \"Smith & Company Ltd.\"\n" + "      },\n"
-                + "      {\n" + "        \"translate\": \"no\",\n" + "        \"content\": \"Smithtown\"\n"
-                + "      }\n" + "    ],\n" + "    \"signature\": [\n" + "      \"Paul Smith\",\n"
-                + "      \"Nina Vuong\"\n" + "    ],\n" + "    \"weblink\": \"http://www.smith-company-ltd.com\",\n"
-                + "    \"logo\": {\n" + "      \"class\": \"11111111111111111\",\n" + "      \"map\": {\n"
-                + "        \"TestKey2\": \"22222TestValue22222\",\n" + "        \"TestKey1\": \"11111TestValue11111\"\n"
-                + "      }\n" + "    },\n" + "    \"salutation\": {\n" + "      \"limit\": 40,\n"
-                + "      \"content\": \"Dear Daniel,\"\n" + "    },\n" + "    \"text\": [\n" + "      {\n"
-                + "        \"component\": [\n" + "          {\n" + "            \"translate\": \"yes\",\n"
-                + "            \"content\": \"XML Parser rules\"\n" + "          },\n" + "          {\n"
-                + "            \"translate\": \"no\",\n" + "            \"content\": \"XML\"\n" + "          }\n"
-                + "        ],\n" + "        \"emphasis\": \"SDL Trados Studio 2009\",\n" + "        \"content\": [\n"
-                + "          \"Thank you for sending us the information on\",\n"
-                + "          \".\\n\\n\\n    We like your products and think they certainly represent the most powerful translation\\nsolution on the market.\\n\\n\\n    We especially like the\",\n"
-                + "          \"options in the\",\n"
-                + "          \"filter.\\n\\n\\n    It has helped us to set up support for our XML files in a flash.\\n\\n\\n    We have already downloaded the latest version from your Customer Center.\"\n"
-                + "        ]\n" + "      },\n"
-                + "      \"We would like to order 50 licenses.\\n\\n\\n    Please send us a quote. Keep up the good work!\"\n"
-                + "    ],\n" + "    \"greetings\": {\n" + "      \"minlength\": 10,\n"
-                + "      \"content\": \"Yours sincerely,\"\n" + "    },\n" + "    \"title\": [\n" + "      {\n"
-                + "        \"maxlength\": 10,\n" + "        \"content\": \"Quote Letter\"\n" + "      },\n"
-                + "      {\n" + "        \"maxlength\": 40,\n" + "        \"content\": \"Quote Details\"\n"
-                + "      }\n" + "    ]\n" + "  },\n" + "  \"letter1\": \"\"\n" + "}";
+        String expectedJsonString = "{\"262_letter\":{\"262_text\":[{\"262_content\":[\"Thank you for sending us the information on\",\".    We like your products and think they certainly represent the most powerful translationsolution on the market.    We especially like the\",\"options in the\",\"filter.    It has helped us to set up support for our XML files in a flash.    We have already downloaded the latest version from your Customer Center.\"],\"262_component\":{\"262_translate\":\"no\",\"262_content\":\"XML\"},\"262_emphasis\":\"SDL Trados Studio 2009\"},\"We would like to order 50 licenses.    Please send us a quote. Keep up the good work!\"],\"262_weblink\":\"http://www.smith-company-ltd.com\",\"262_salutation\":{\"262_content\":\"Dear Daniel,\",\"262_limit\":40},\"262_title\":{\"262_maxlength\":40,\"262_content\":\"Quote Details\"},\"262_greetings\":{\"262_minlength\":10,\"262_content\":\"Yours sincerely,\"},\"262_logo\":{\"262_alt\":\"Logo of Smith and Company Ltd.\",\"262_address\":\"http://www.smith-company-ltd.com/logo.jpg\"},\"262_address\":{\"262_translate\":\"no\",\"262_content\":\"Smithtown\"},\"262_signature\":[\"Paul Smith\",\"Nina Vuong\"]}}\n";
         StringReader s = new StringReader(originalXml);
-        JSONPointer jp = new JSONPointer("/letter/logo");
 
+        XML.func keyTransformer = (String str) -> {
+            return "262_" + str;
+       };
         JSONObject expectedJson = new JSONObject(expectedJsonString);
-        JSONObject replacement = new JSONObject();
-        replacement.put("class", "11111111111111111");
-        Map m = new LinkedHashMap(4);
-        m.put("TestKey1", "11111TestValue11111");
-        m.put("TestKey2", "22222TestValue22222");
-        replacement.put("map", m);
-        JSONObject actualJson = XML.toJSONObject(s, jp, replacement);
-        System.out.println(actualJson);
-        Util.compareActualVsExpectedJsonObjects(actualJson, expectedJson);
+
+		long startTime = System.currentTimeMillis();
+        JSONObject actualJson = XML.toJSONObject(s, keyTransformer);
+		long finishTime = System.currentTimeMillis();
+        System.out.println(expectedJson.toString());
+        System.out.println(actualJson.toString());
+		System.out.println("That took: " + (finishTime - startTime) + " ms");
+		Util.compareActualVsExpectedJsonObjects(actualJson, expectedJson);
     }
-
-
-
-//		String test = "<signature>Nina Vuong</signature>";
-//		JSONObject e =  XML.toJSONObject(test);
-//
-//		System.out.println(e);
-
-
-
-        //String expectation = ""
-//        StringReader s = new StringReader(originalXml);
-//        XML.func keyTransformer = (String str) -> {
-//            return "262_" + str;
-      // };
-        //JSONObject expectedJson = XML.toJSONObject(originalXml);
-        //JSONObject actualJson = XML.toJSONObject(s, keyTransformer);
-        //System.out.println(expectedJson);
-       // System.out.println(actualJson.toString());
-        //Util.compareActualVsExpectedJsonObjects(actualJson, expectedJson);
-
-   // }
-
 }
